889cfa2bc70c135f676227683625edd33e3ee701
Helgi Steinarr <HelgiSteinarr@users.noreply.github.com>
2018-09-12 17:50:51 +0000

    Add files via upload

---
+++ verk1/scriptur/playerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class playerController : MonoBehaviour {
+
+    // Public svo ég get sett myndavelina og agentinn við scriptið ínní editornum
+    public Camera cam;
+    public NavMeshAgent agent;
+
+	void Update () {
+		// mouse button 0 er left-click
+		if (Input.GetMouseButtonDown(0))
+        {
+			// Tekur púnktin sem músin er á skjánum og breytir því í "ray"
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+			
+            if (Physics.Raycast(ray, out hit))
+            {
+				// Setur áfangastað agentsins á punktinn sem ray skotið lendir
+                agent.SetDestination(hit.point);
+            }
+        }
+	}
+}
